80b67948c699473e7fe292d9bacefc776e4ed2ef
becca <rebecca.kaci@gmail.com>
2022-02-01 14:29:01 +0100

    création de la structure de l'app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+import Home from './pages/Home'
+import GameBoard from './pages/GameBoard'
+import Profile from './pages/Profile'
+import AuctionRoom from './pages/AuctionRoom'
+import NotFound from './pages/NotFound'
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+          <Route exact path="/" element={ <Home /> } />
+          <Route path="/gameboard" element={ <GameBoard /> } />
+          <Route path="/profile" element={ <Profile /> } />
+          <Route path="/auction-room" element={ <AuctionRoom /> } />
+          <Route path="*" element={ <NotFound /> } />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
