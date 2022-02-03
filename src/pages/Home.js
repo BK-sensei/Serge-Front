4f457aab0e069a31c64fb6e68b542d6ed014ea55
becca <rebecca.kaci@gmail.com>
2022-02-03 16:52:23 +0100

    style homepage connexion done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,40 @@
 import React from 'react'
+import TitleWebSite from '../components/TitleWebSite.js'
+import ('../styles/pages-style/home.css')
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className='home-container'>
+            <TitleWebSite />
+
+            <div className='bg-golden'>
+                <div className='bg-bronze'>
+                    <h2>Connexion</h2>
+                    <form>
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            // placeholder='serge@subway.com'
+                        />
+                        <label>Password</label>
+                        <input
+                            type="password"
+                        />
+                    </form>
+                    <button 
+                        type='submit'
+                        className='ok-btn'
+                    >OK</button>
+                </div>
+            </div>
+
+            <div className='bg-blue signup-div'>
+                <h3>Pas de compte ?</h3>
+                <button 
+                    type='button'
+                    className='signup-btn'
+                >Inscription</button>
+            </div>
         </div>
     )
 }
