a632e8bac19fc493f3b2432f1212a2dddfd57d58
becca <rebecca.kaci@gmail.com>
2022-02-04 10:42:02 +0100

    création de tous les fetch authentication

---
+++ src/api/auth.js
@@ -0,0 +1,70 @@
+const logIn = async ({ email, password }) => {
+    const loginResponse = await fetch('http://localhost:5000/auth/login', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+            email,
+            password
+        })
+    })
+
+    if (loginResponse.status >= 400) {
+        throw loginResponse.statusText
+    }
+
+    const data = await loginResponse.json()
+    return data
+}
+
+const signUp = async ({ username, email, password }) => {
+    const signupResponse = await fetch('http://localhost:5000/auth/signup', {
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        credentials: 'include',
+        body: JSON.stringify({
+            username,
+            email,
+            password
+        })
+    })
+
+    const data = await signupResponse.json()
+    return data
+}
+
+const getMe = async () => {
+    const getmeResponse = await fetch('http://localhost:5000/auth/me', {
+        headers: {
+            'Content-type': 'application/json'
+        },
+        credentials: 'include',
+    })
+
+    const data = getmeResponse.json()
+    return data
+}
+
+const logOut = async () => {
+    const logoutResponse = await fetch('http://localhost:5000/auth/logout', {
+        method: 'delete',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        credentials: 'include',
+    })
+
+    const data = logoutResponse.json()
+    return data
+}
+
+export {
+    logIn,
+    signUp,
+    getMe,
+    logOut
+}
