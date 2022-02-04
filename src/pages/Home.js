ad614e4246db8e0d8bb043817ecee9555ca10727
Edouard-Germain <edangermain@gmail.com>
2022-02-04 10:30:10 +0100

    first step

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
+import SideBar from '../components/SideBar'
+
 const Home = () => {
     return (
         <div>
-            Home
+            <SideBar/>
         </div>
     )
 }
