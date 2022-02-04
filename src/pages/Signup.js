f738fa84f8e301db73f226249a7b60eedb0dfde3
becca <rebecca.kaci@gmail.com>
2022-02-04 11:30:18 +0100

    tentative pour faire fonctionner les forms d'inscription et de connexion

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,8 +1,56 @@
-import React from 'react'
+import { useContext } from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import { useNavigate } from 'react-router-dom'
+
+import { UserContext } from '../contexts/User.js'
+import { logIn, signUp } from '../api/auth'
+
 import TitleWebSite from '../components/TitleWebSite.js'
 import ('../styles/pages-style/home.css')
 
 const Signup = () => {
+    const navigate = useNavigate()
+    const { setUser } = useContext(UserContext)
+
+    const { values, errors, handleSubmit, handleChange } = useFormik({
+        initialValues: {
+            username: '',
+            email: '',
+            password: '',
+            passwordConfirmation: '',
+            avatar: ''
+        },
+        onSubmit: async (values, { setFielError }) => {
+            const { username, password } = values
+
+            const response = await signUp({
+                username,
+                password
+            })
+
+            if (response.error) {
+                setFielError('submit', response.error)
+            } else {
+                const user = await logIn({ username, password })
+                setUser(user)
+                navigate('/gameboard')
+            }
+        },
+        validationSchema: Yup.object({
+            pseudo: Yup.string()
+            .required("Pseudo obligatoire"),
+            email: Yup.string()
+            .required("Email obligatoire")
+            .email("Email non conforme"),
+            password: Yup.string()
+            .required("Mot de passe obligatoire"),
+            passwordConfirmation: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Le mot de passe doit être le même'),
+        }),
+        validateOnChange: false,
+    })
+
     return (
         <div className='home-container'>
             <TitleWebSite />
@@ -10,11 +58,14 @@ const Signup = () => {
             <div className='bg-golden'>
                 <div className='bg-bronze'>
                     <h2>Inscription</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label>Pseudo</label>
                         <input
                             type="text"
-                            name='pseudo'
+                            name='username'
+                            onChange={handleChange}
+                            value ={values.username}
+                            error={errors.username}
                         />
                         <label>Email</label>
                         <input
@@ -25,11 +76,17 @@ const Signup = () => {
                         <input
                             type="password"
                             name='password'
+                            onChange={handleChange}
+                            value ={values.password}
+                            error={errors.password}
                         />
                         <label>Confirmer votre mot de passe</label>
                         <input
                             type="password"
-                            name='password'
+                            name='passwordConfirmation'
+                            onChange={handleChange}
+                            value ={values.passwordConfirmation}
+                            error={errors.passwordConfirmation}
                         />
                         <label>Choisir un avatar</label>
                         <div className='chooseAnAvatar'>
