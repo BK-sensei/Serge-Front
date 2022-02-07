4464b2cc5bc289be0c116bddc6dc102a7b4448cd
HeleneSoubeyrand <helenesoubi@gmail.com>
2022-02-07 17:44:45 +0100

    cardProperty fond blanc

--- src/components/CardProperty.js
+++ src/components/CardProperty.js
@@ -5,39 +5,44 @@ const CardProperty = () => {
         <div className="cardContainer">
             <div className="cardBorder">
                 <div className="cardContent">
+
                     <div className="titleContent">
-                        <div className="titleType">
-                            <p>Titre de propriété</p>
-                        </div>
-                        <div className="titleProperty">
-                            <p>Titre de propriété</p>
-                        </div>
+                        <p className="titleType">Titre de propriété</p>
+                        <p className="titleProperty">Titre de propriété</p>
+                    </div>
+
+                    <div className="listContent">
+                        <ul>
+                            <li>
+                                <p>Valeur NFT</p>
+                                <p>8000 $</p>
+                            </li>
+                            <li>
+                                <p>Taxe de passage</p>
+                                <p>600 $</p>
+                            </li>
+                        </ul>
+                        <ul>
+                            <li>
+                                <p>Distributeur</p>
+                                <p>800 $</p>
+                            </li>
+                            <li>
+                                <p>Boutique</p>
+                                <p>4000 $</p>
+                            </li>
+                            <li>
+                                <p>Centre commercial</p>
+                                <p>8000 $</p>
+                            </li>
+                        </ul>
                     </div>
                     
-                    <div>
-                        <p>Valeur NFT</p>
-                        <p>8000 $</p>
-                    </div>
-                    <div>
-                        <p>Taxe de passage</p>
-                        <p>600 $</p>
-                    </div>
-                    <div>
-                        <p>Distributeur</p>
-                        <p>800 $</p>
-                    </div>
-                    <div>
-                        <p>Boutique</p>
-                        <p>4000 $</p>
-                    </div>
-                    <div>
-                        <p>Centre commercial</p>
-                        <p>8000 $</p>
-                    </div>
-                    <div>
+                    <div className="buttons">
                         <button type="button">Améliorer</button>
                         <button type="button">Vendre</button>
                     </div>
+
                 </div>
             </div>       
     </div>
